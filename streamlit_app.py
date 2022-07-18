3add6e0b8b98f6d66e5db5b9d2796a00f53a97aa
Pranita Patil <109273793+PranitaSantoshPatil@users.noreply.github.com>
2022-07-18 13:10:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('Kale,spinach and rocket smoothei')
 streamlit.text('🥚 🐔Hard-boiled free range egg')
 streamlit.text('Avacado Toast')
 streamlit.header('🥭🍌Biuld Your Own Fruit Smoothie🥝')
+import pandas
+my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

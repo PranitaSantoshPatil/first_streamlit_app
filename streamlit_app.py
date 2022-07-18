6d158c6f91299a017ccd8d438e32b11916c218a0
Pranita Patil <109273793+PranitaSantoshPatil@users.noreply.github.com>
2022-07-18 12:05:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+
+
 Streamlit.header('Breakfast Menu')
 Streamlit.text('Omega3 and blueberry oatmeal')
 streamlit.text('Kale,spinach and rocket smoothei')

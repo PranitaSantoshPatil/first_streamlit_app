3b2e0dfa22bcdd04fa4d0aa8ce5e0a3bba0c4e61
Pranita Patil <109273793+PranitaSantoshPatil@users.noreply.github.com>
2022-07-18 12:25:28 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 
-Streamlit.header('Breakfast Menu')
-Streamlit.text('Omega3 and blueberry oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and blueberry oatmeal')
 streamlit.text('Kale,spinach and rocket smoothei')
 streamlit.text('Hard-boiled free rane egg')

60bb43bc1785a05645409904a2d652006741a55d
Pranita Patil <109273793+PranitaSantoshPatil@users.noreply.github.com>
2022-08-04 17:33:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')                                     
 streamlit.text('Omega3 and blueberry oatmeal')
 streamlit.text('Kale,spinach and rocket smoothei')
-streamlit.text('🥚 🐔Hard-boiled free range egg')
+streamlit.text('🥚 🐔Hard-boiled free-range egg')
 streamlit.text('Avacado Toast')
 streamlit.header('🥭🍌Biuld Your Own Fruit Smoothie🥝')
 #import pandas
